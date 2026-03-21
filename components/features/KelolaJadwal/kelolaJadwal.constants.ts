c9031749427b4223242db386af75036ce3f140c9
Mazyaa <muhamadazisudarya@unbaja.ac.id>
2026-03-21 19:02:54 +0700

    feat: created hooks for manage schedule

---
+++ components/features/KelolaJadwal/kelolaJadwal.constants.ts
@@ -0,0 +1,11 @@
+const LISTS_KELOLA_JADWAL = [
+  { name: "NAMA TRAINING", uid: "nama_training" },
+  { name: "HARI KE", uid: "hari_ke" },
+  { name: "TANGGAL", uid: "tanggal" },
+  { name: "AKTIVITAS", uid: "aktivitas" },
+  { name: "NAMA INSTRUKTUR", uid: "nama_instruktur" },
+  { name: "NAMA ASESOR", uid: "nama_asesor" },
+  { name: "AKSI", uid: "aksi" },
+];
+
+export default LISTS_KELOLA_JADWAL;
---
+++ types/kelolaJadwal.d.ts
@@ -0,0 +1,8 @@
+export interface IKelolaJadwal {
+  id: string;
+  trainingId: string;
+  startDate: Date;
+  duration: number;
+  meetingLink: string;
+  batch: string;
+}
