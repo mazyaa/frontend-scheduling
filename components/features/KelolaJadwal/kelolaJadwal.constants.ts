ba809a8c605aa6109d6baf65bc1c72a5b0305fb2
Mazyaa <muhamadazisudarya@unbaja.ac.id>
2026-03-23 03:27:46 +0700

    fix: fixing mapping data

--- components/features/KelolaJadwal/kelolaJadwal.constants.ts
+++ components/features/KelolaJadwal/kelolaJadwal.constants.ts
@@ -1,8 +1,8 @@
 const LISTS_KELOLA_JADWAL = [
   { name: "NAMA TRAINING", uid: "nama_training" },
-  { name: "TANGGAL MULAI", uid: "tanggal_mulai" },
-  { name: "DURASI TRAINING", uid: "durasi_training" },
-  { name: "LINK MEETING TRAINING", uid: "link_meeting_training" },
+  { name: "TANGGAL MULAI", uid: "startDate" },
+  { name: "DURASI TRAINING", uid: "duration" },
+  { name: "LINK MEETING TRAINING", uid: "meetingLink" },
   { name: "BATCH", uid: "batch" },
   { name: "AKSI", uid: "aksi" },
 ];
